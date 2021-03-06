34f3f24bfa6fc2ee450f7f79235850f607a55f59
Allison Dietz <dietz.allison@outlook.com>
2021-03-06 10:00:57 -0700

    Add basic heading structure

--- src/Components/MainHeading/MainHeading.js
+++ src/Components/MainHeading/MainHeading.js
@@ -3,3 +3,22 @@ import { Route }from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './MainHeading.css';
 import SearchBar from '../SearchBar/SearchBar';
+
+const MainHeading = () => {
+  return (
+    <header className="heading">
+    <h1 className="heading-title">EdibleEarth</h1>
+    <h2 className="heading-subtitle">Can I eat this plant?</h2>
+    <Route exact path='/' 
+      render={ () => { 
+        return <SearchBar />
+      }}/>
+    </header>
+  )
+}
+
+MainHeading.propTypes = {
+
+}
+
+export default MainHeading;
