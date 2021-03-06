ec70faab11fd5db61b58be26c7f099ca5a701d95
Allison Dietz <dietz.allison@outlook.com>
2021-03-06 10:06:42 -0700

    Add basic PlantCard structure

---
+++ src/Components/PlantCard/PlantCard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './PlantCard.css';
+
+const PlantCard = () => {
+
+  }
+
+  return (
+    <article className='plant-card'>
+    </article>
+  )
+}
+
+PlantCard.propTypes = {
+
+}
+
+export default PlantCard;
