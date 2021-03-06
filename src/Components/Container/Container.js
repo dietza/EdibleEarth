24d7217d3502dde6d8c742b2373a9df7c0ad2341
Allison Dietz <dietz.allison@outlook.com>
2021-03-06 15:31:30 -0700

    Add basic Container structure

---
+++ src/Components/Container/Container.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import PlantCard from '../PlantCard/PlantCard';
+import './Container.css';
+
+const Container = () => {
+
+  return (
+    <section className='plants-container'>
+
+    </section>
+  )
+}
+
+Container.propTypes = {
+
+}
+
+export default Container;
