e7bb6092b37ba1a03ad5e3954e7b1483c3396099
Allison Dietz <dietz.allison@outlook.com>
2021-03-06 09:54:11 -0700

    Update initial file structure

---
+++ src/Components/App/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import MainHeading from '../MainHeading/MainHeading';
+import SearchBar from '../SearchBar/SearchBar';
+import Footer from '../Footer/Footer';
+import Container from '../Container/Container';
+import PlantCard from '../PlantCard/PlantCard';
+import PlantDetails from '../PlantDetails/PlantDetails';
+import DetailsView from '../DetailsView/DetailsView';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/MainHeading/MainHeading.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { Route }from 'react-router-dom';
+import PropTypes from 'prop-types';
+import './MainHeading.css';
+import SearchBar from '../SearchBar/SearchBar';
