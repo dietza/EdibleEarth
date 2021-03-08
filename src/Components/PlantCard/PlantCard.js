9341055d4d4770a9059e2cbd062514b9910a2119
Allison Dietz <dietz.allison@outlook.com>
2021-03-08 11:36:13 -0700

    Add display of fetched data to fill out individual plant cards

--- src/Components/PlantCard/PlantCard.js
+++ src/Components/PlantCard/PlantCard.js
@@ -2,14 +2,19 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 import './PlantCard.css';
 
-const PlantCard = () => {
+const PlantCard = ({ info }) => {
+  const plantCommonName = info.common_name;
+  const scientificName = info.scientific_name;
 
   return (
     <article className='plant-card'>
-      <section>
-        <h3>Plant Name</h3>
-        <p>There would actually be a plant image here...</p>
+      <section className="card-details">
+        <h2 className="plant-common-name">{`${plantCommonName}`}</h2>
+        <p className="plant-scientific-name">{`${scientificName}`}</p>
       </section>
+      <img src={ info.image_url } 
+      alt={`${scientificName} known as ${plantCommonName}`}
+      className="plant-card-image"/>
     </article>
   )
 }
